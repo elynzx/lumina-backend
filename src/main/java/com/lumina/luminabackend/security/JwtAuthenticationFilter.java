490db7d8cac1f0f0b567ff056c026434a625e6b8
Evelyn Pascual C <153583839+elynzx@users.noreply.github.com>
2025-10-15 09:29:35 -0500

    update: use NonNullApi annotations to JwtAuthenticationFilter method parameters

--- src/main/java/com/lumina/luminabackend/security/JwtAuthenticationFilter.java
+++ src/main/java/com/lumina/luminabackend/security/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -31,8 +32,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
      * and sets the authentication in the SecurityContext.
      */
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
 
         String token = getTokenFromRequest(request);
 
