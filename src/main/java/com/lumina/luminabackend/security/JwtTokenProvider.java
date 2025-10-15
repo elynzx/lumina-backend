dc11dd6983097685230871b267e1a5b8bc50f370
Evelyn Pascual C <153583839+elynzx@users.noreply.github.com>
2025-10-15 07:42:26 -0500

    feat: add JwtTokenProvider for JWT creation and validation

---
+++ src/main/java/com/lumina/luminabackend/security/JwtTokenProvider.java
@@ -0,0 +1,66 @@
+package com.lumina.luminabackend.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    // secret key
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    // expiration ms
+    @Value("${jwt.expiration}")
+    private long jwtExpirationInMs;
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+
+    // Generate token
+    public String generateToken(Authentication authentication) {
+        CustomUserPrincipal userPrincipal = (CustomUserPrincipal) authentication.getPrincipal();
+        Date expiryDate = new Date(System.currentTimeMillis() + jwtExpirationInMs);
+
+        return Jwts.builder()
+                .subject(userPrincipal.getUsername())
+                .issuedAt(new Date())
+                .expiration(expiryDate)
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    // Extract username/email from token
+    public String getUsernameFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        return claims.getSubject();
+    }
+
+    // Validate token
+    public boolean validateToken(String authToken) {
+        try {
+            Jwts.parser()
+                    .verifyWith(getSigningKey())
+                    .build()
+                    .parseSignedClaims(authToken);
+            return true;
+        } catch (JwtException ex) {
+            System.out.println("Invalid JWT token: " + ex.getMessage());
+        } catch (IllegalArgumentException ex) {
+            System.out.println("JWT claims string is empty");
+        }
+        return false;
+    }
+}
