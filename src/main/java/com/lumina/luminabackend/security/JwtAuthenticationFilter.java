68540c8b10dfdf858df62a874ea5adf8bc39b3e3
Evelyn Pascual C <153583839+elynzx@users.noreply.github.com>
2025-10-15 07:42:44 -0500

    feat: add JwtAuthenticationFilter with token extraction and authentication

---
+++ src/main/java/com/lumina/luminabackend/security/JwtAuthenticationFilter.java
@@ -0,0 +1,63 @@
+package com.lumina.luminabackend.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider tokenProvider; // JWT utils
+    private final UserDetailsService userDetailsService; // load user infor from db
+
+    /**
+     * Main filter JWT check + authentication
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String token = getTokenFromRequest(request); // extract token
+
+        if (StringUtils.hasText(token) && tokenProvider.validateToken(token)) { // validate
+            String username = tokenProvider.getUsernameFromToken(token); // get user email
+
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username); // load user
+
+            // authentication token
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContextHolder.getContext().setAuthentication(authentication); // set security context
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    /**
+     * Extract JWT from Authorization header
+     */
+    private String getTokenFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization"); // get header
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) { // check Bearer
+            return bearerToken.substring(7); // remove prefix
+        }
+        return null;
+    }
+}
